47849ece4a4c516f8fb1c176417e2f15b2aef108
Torsten Feld <torsten.feld@avira.com>
2013-06-29 12:57:30 +0200

    easy-checkNums successfully finished

---
+++ easy-checkNums.py
@@ -0,0 +1,18 @@
+
+
+
+def compare_numbers(num1, num2):
+    if num1 < num2:
+        return 'true'
+    elif num1 > num2:
+        return 'false'
+    else:
+        return '-1'
+
+
+
+
+
+num1 = 5
+num2 = 3
+print compare_numbers(num1, num2)

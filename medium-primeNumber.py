b02ff534984a28757613c83445ddc23619633b90
Torsten Feld <torsten.feld@avira.com>
2013-06-29 15:32:29 +0200

    renamed on level

--- medium-primeNumber.py
+++ medium-primeNumber.py
@@ -1,26 +1,17 @@
 
 
-def PrimeMover(num):
-    primeNumbers = [2,3]
-    isPrime = 1
+
+def CheckForPrime(num):
+    maxDivider = num / 2
+    if num % 2 != 0:
+        maxDivider = (num - 1) / 2
     
-    for i in range(4, 10001):
-        maxDivider = i / 2
-        if i % 2 != 0:
-            maxDivider = (i - 1) / 2
+    for i in range(2, maxDivider + 1):
+        if num % i == 0:
+            return 'false'
+    return 'true'
+        
     
-        for j in range(2, maxDivider + 1):
-            if i % j == 0:
-                isPrime = 0
-                break;
-
-        if isPrime == 1:
-            primeNumbers.append(i)
-            if len(primeNumbers) == num:
-                return i
-
-        isPrime = 1
-
-
-num = 100
-print PrimeMover(num)
+    
+num = 12
+print CheckForPrime(num)
